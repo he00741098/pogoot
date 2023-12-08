9688802914b6abab73cf931a43924497de5a3386
Jesse He <heqiqi2014@hotmail.com>
2023-12-08 16:26:03 +0000

    stiff

--- pogootRefactored/src/dataTypes/database.rs
+++ pogootRefactored/src/dataTypes/database.rs
@@ -19,22 +19,30 @@ impl Database{
     pub fn new(config:secrets)->Self{
         Database {  }
     }
+    ///Generates a unique user Id that is not present in the table
     pub async fn generate_uuid(&self)->String{
         todo!()
     }
+    ///Checks if the username is currently in the database
     pub async fn check_username_exists(&self, username:String)->bool{
         todo!()
     }
+    ///Stores userdata in the database
     pub async fn store_user_data(&mut self, userData:UserData)->Result<(),()>{
         todo!()
     }
+    ///Gets userdata from the database
     pub async fn fetch_user_data(&self, username:Option<String>, uuid:Option<String>)->Result<UserData, ()>{
         todo!()
     }
-    pub async fn store_note_card_set(&self, id:String, questionsList:questionList){
+    ///Stores note card and returns generated id
+    pub async fn store_note_card_set(&self, questionsList:questionList, username:String)->String{
         todo!()
     }
-    pub async fn edit_note_card_set(&self, id:String, editedList:questionList){
+    ///Takes an ID and full list of questions. Replaces current entry. If current entry does not exist,
+    /// it will add a new entry
+    /// Requires user to have own or have permissions to the set
+    pub async fn edit_note_card_set(&self, id:String, editedList:questionList, token:String){
         todo!()
     }
 }
--- pogootRefactored/src/notecard.rs
+++ pogootRefactored/src/notecard.rs
@@ -5,3 +5,25 @@
 //require login
 //
 //allow conversion to pogoot game
+
+use std::sync::Arc;
+
+use crate::dataTypes::{state_storage::state_storage, questionList};
+
+///Takes the user's token and stores the questions list in the database
+///Token must be valid. Returns the ID of the stored questionlist.
+/// Adds set to userdata
+pub async fn upload_set(token:String, questions:questionList, state:Arc<state_storage>)->String{
+    todo!()
+}
+
+///Returns the set that has the id
+pub async fn get_set(id:String, state:Arc<state_storage>)->questionList{
+    todo!()
+}
+
+///Returns all the ids of a user's sets
+pub async fn get_users_sets(username:String, state:Arc<state_storage>)->Vec<String>{
+todo!()
+}
+
